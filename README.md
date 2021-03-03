3da645fe2d3063f2ba1d50a41a7a2a0c629aac5c
brandonism <6565206+brandonism@users.noreply.github.com>
2021-03-02 21:37:20 -0500

    garbage plate locator basic

--- README.md
+++
@@ -1,1 +0,0 @@
-# getmegarbage.github.io
---
+++ precache-manifest.1f921e5927f5249c52e795f52e2a0f72.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c027323bc9bae49331bf09084a4671c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af4ea7c075510d27ccef",
+    "url": "/static/css/2.ff645aaf.chunk.css"
+  },
+  {
+    "revision": "a2e2f192752332d7c3af",
+    "url": "/static/css/main.d6b65cd3.chunk.css"
+  },
+  {
+    "revision": "af4ea7c075510d27ccef",
+    "url": "/static/js/2.ae32443b.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/static/js/2.ae32443b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2e2f192752332d7c3af",
+    "url": "/static/js/main.a99895cf.chunk.js"
+  },
+  {
+    "revision": "73d04a04d86a76ceb22b",
+    "url": "/static/js/runtime-main.f2a3b6bb.js"
+  }
+]);
